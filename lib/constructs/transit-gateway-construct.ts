3f9e00640ccd2bc1128893fbcb6a7a6a9e554fe3
jmetzdorf7 <jmetzdorf7@gmail.com>
2025-10-07 10:27:47 -0400

    Update transit-gateway-construct.ts
    code reviewed and refactored

--- lib/constructs/transit-gateway-construct.ts
+++ lib/constructs/transit-gateway-construct.ts
@@ -1,47 +1,36 @@
 import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { aws_ec2 as ec2, CfnOutput, Tags } from 'aws-cdk-lib';
+
+export interface TransitGatewayConstructProps {
+  envName: string;
+  amazonSideAsn?: number;
+  autoAcceptSharedAttachments?: string;
+  defaultRouteTableAssociation?: string;
+  defaultRouteTablePropagation?: string;
+  tags?: { key: string; value: string }[];
+}
 
 export class TransitGatewayConstruct extends Construct {
   public readonly transitGateway: ec2.CfnTransitGateway;
 
   constructor(scope: Construct, id: string, props: TransitGatewayConstructProps) {
-  super(scope, id);
+    super(scope, id);
 
-  this.transitGateway = new ec2.CfnTransitGateway(this, `TransitGateway-${props.envName}`, {
-    description: `Transit Gateway for ${props.envName} environment`,
-    ...
-  amazonSideAsn: 64512,
-      autoAcceptSharedAttachments: 'enable',
-      defaultRouteTableAssociation: 'enable',
-      defaultRouteTablePropagation: 'enable',
+    this.transitGateway = new ec2.CfnTransitGateway(this, `TransitGateway-${props.envName}`, {
+      description: `Transit Gateway for ${props.envName} environment`,
+      amazonSideAsn: props.amazonSideAsn ?? 64512,
+      autoAcceptSharedAttachments: props.autoAcceptSharedAttachments ?? 'enable',
+      defaultRouteTableAssociation: props.defaultRouteTableAssociation ?? 'enable',
+      defaultRouteTablePropagation: props.defaultRouteTablePropagation ?? 'enable',
+      tags: props.tags ?? [
+        { key: 'Name', value: `TransitGateway-${props.envName}` },
+        { key: 'Environment', value: props.envName },
+      ],
     });
-    }
 
-    // Environment naming
-    export interface TransitGatewayConstructProps {
-    envName: string;
-    }
-    
-    // Tagging
-    tags: [
-    {
-    key: 'Name',
-    value: 'TransitGateway',
-    },
-    {
-    key: 'Environment',
-    value: 'dev',
-    },
-    ],
-
-    // Outputs
-    new cdk.CfnOutput(this, 'TransitGatewayId', {
-    value: this.transitGateway.ref,
+    new CfnOutput(this, 'TransitGatewayId', {
+      value: this.transitGateway.ref,
+      exportName: `TransitGatewayId-${props.envName}`,
     });
   }
 }
-
-
-
-
-
