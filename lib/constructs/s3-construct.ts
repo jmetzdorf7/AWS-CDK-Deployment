ad5f4b417c3d61cfd7e133d617b6c1ab8802d908
jmetzdorf7 <jmetzdorf7@gmail.com>
2025-10-07 10:26:34 -0400

    Update s3-construct.ts
    code reviewed and refactored

--- lib/constructs/s3-construct.ts
+++ lib/constructs/s3-construct.ts
@@ -1,21 +1,33 @@
 import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Bucket, BucketProps, RemovalPolicy, BucketEncryption, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 
 export interface S3ConstructProps {
   envName: string;
+  bucketProps?: Partial<BucketProps>; // Allow extensibility for future bucket options
 }
 
+/**
+ * S3Construct creates an S3 bucket configured for the specified environment.
+ * - In production: retains the bucket and disables auto-delete for safety.
+ * - In other environments: destroys bucket and auto-deletes objects for easy cleanup.
+ */
 export class S3Construct extends Construct {
-  public readonly bucket: s3.Bucket;
+  public readonly bucket: Bucket;
 
   constructor(scope: Construct, id: string, props: S3ConstructProps) {
     super(scope, id);
 
-    this.bucket = new s3.Bucket(this, `Bucket-${props.envName}`, {
-      bucketName: `cdk-${props.envName}-bucket-${this.node.addr}`,
+    // Sanitize envName and node.addr if needed for S3 bucket naming rules
+    const bucketName = `cdk-${props.envName.toLowerCase()}-bucket-${this.node.addr}`.replace(/_/g, '-');
+
+    this.bucket = new Bucket(this, `Bucket-${props.envName}`, {
+      bucketName,
       versioned: true,
-      removalPolicy: props.envName === 'prod' ? undefined : s3.RemovalPolicy.DESTROY,
+      encryption: BucketEncryption.S3_MANAGED,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: props.envName === 'prod' ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
       autoDeleteObjects: props.envName !== 'prod',
+      ...props.bucketProps, // Allow overriding defaults
     });
   }
 }
