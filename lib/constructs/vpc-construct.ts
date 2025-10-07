0ede06cccfcc0b404cc785135aa7c50ee0b7e682
jmetzdorf7 <jmetzdorf7@gmail.com>
2025-10-07 10:28:14 -0400

    Update vpc-construct.ts
    code reviewed and refactored

--- lib/constructs/vpc-construct.ts
+++ lib/constructs/vpc-construct.ts
@@ -1,6 +1,11 @@
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cdk from 'aws-cdk-lib';
+
 export interface VpcConstructProps {
   envName: string;
   maxAzs?: number;
+  natGateways?: number; // Optional: allow custom NAT Gateway count
 }
 
 export class VpcConstruct extends Construct {
@@ -9,9 +14,14 @@ export class VpcConstruct extends Construct {
   constructor(scope: Construct, id: string, props: VpcConstructProps) {
     super(scope, id);
 
+    // Validation should happen first
+    if (!props.envName) {
+      throw new Error('envName is required for VpcConstruct');
+    }
+
     this.vpc = new ec2.Vpc(this, `Vpc-${props.envName}`, {
       maxAzs: props.maxAzs ?? 2,
-      natGateways: 1,
+      natGateways: props.natGateways ?? 1,
       subnetConfiguration: [
         {
           name: `${props.envName}-Public`,
@@ -24,21 +34,17 @@ export class VpcConstruct extends Construct {
         {
           name: `${props.envName}-Isolated`,
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-        }
-        // Tagging
-        cdk.Tags.of(this.vpc).add('Environment', props.envName);
-        // Validation
-        if (!props.envName) {
-        throw new Error('envName is required for VpcConstruct');
-        // Outputs
-        new cdk.CfnOutput(this, 'VpcId', {
-        value: this.vpc.vpcId);  
-        }
+        },
       ],
+    });
+
+    // Tagging
+    cdk.Tags.of(this.vpc).add('Environment', props.envName);
+
+    // Outputs
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: this.vpc.vpcId,
+      exportName: `${props.envName}-VpcId`,
     });
   }
 }
-
-
-
-
